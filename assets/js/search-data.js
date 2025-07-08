e12096b43c2729271ca4c5476d971b39c2c52ce0
belgioio <giuseppe.belgioioso@gmail.com>
2025-07-08 12:31:49 +0000

    Deploying to gh-pages from @ belgioio/belgioio.github.io@deb612030cd847e6c6831f656df3a1d343ca8983 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -494,7 +494,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%67%69%75%62%65%6C@%6B%74%68.%73%65", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -515,7 +515,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=ck0wft8AAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
