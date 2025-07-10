d0262a5892b76395d4a20ef17a4124c52e977ff2
belgioio <giuseppe.belgioioso@gmail.com>
2025-07-10 20:28:02 +0000

    Deploying to gh-pages from @ belgioio/belgioio.github.io@280eee8f95c5624906f09bcb642e6cdd1d8e1de6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,7 +17,7 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-projects",
-          title: "Projects",
+          title: "projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
