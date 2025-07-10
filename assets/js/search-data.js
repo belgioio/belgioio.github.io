05789158765d5eeb8ab93ee9c40b1985f5e8eb4b
belgioio <giuseppe.belgioioso@gmail.com>
2025-07-10 13:12:30 +0000

    Deploying to gh-pages from @ belgioio/belgioio.github.io@838eda2bf7b842d2b89db31c282616d2d31ee8fc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,8 +23,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-team",
-          title: "Team",
+        },{id: "nav-my-team",
+          title: "my team",
           description: "I am privileged to work with an outstanding research team! We&#39;re a rapidly growing with everyone being either formally co-advised or being integrated through various collaborations.",
           section: "Navigation",
           handler: () => {
