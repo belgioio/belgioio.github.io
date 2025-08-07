36983ab36dfe2aab0640ccfe7f7038892b5c052b
belgioio <giuseppe.belgioioso@gmail.com>
2025-08-07 08:02:49 +0000

    Deploying to gh-pages from @ belgioio/belgioio.github.io@80f31a72b51cd657fb2cd51843b3bdd1c9f4805e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -412,27 +412,7 @@ ninja.data = [{
           section: "News",},{id: "news-i-ve-launched-my-personal-website-still-under-construction-️",
           title: 'I’ve launched my personal website! ✨ Still under construction 🛠️',
           description: "",
-          section: "News",},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{id: "projects-maestro",
+          section: "News",},{id: "projects-maestro",
           title: 'MAESTRO',
           description: "Multi-Agent Energy Systems in Transmission Grid Real-time Operation",
           section: "Projects",handler: () => {
