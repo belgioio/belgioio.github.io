b5b3c102122dc7425808246e054fecf3e3d42be4
belgioio <giuseppe.belgioioso@gmail.com>
2025-08-07 07:41:57 +0000

    Deploying to gh-pages from @ belgioio/belgioio.github.io@8abc898ecaa64fe2ea9e3e61cbb56d9d7336c068 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -412,17 +412,7 @@ ninja.data = [{
           section: "News",},{id: "news-i-ve-launched-my-personal-website-still-under-construction-️",
           title: 'I’ve launched my personal website! ✨ Still under construction 🛠️',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
+          section: "News",},{id: "projects-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
