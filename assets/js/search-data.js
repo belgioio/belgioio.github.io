8fa4022c6e5fe630a76cfc59ee70a36fffead667
belgioio <giuseppe.belgioioso@gmail.com>
2025-08-07 10:46:42 +0000

    Deploying to gh-pages from @ belgioio/belgioio.github.io@136cec62c3bfdab9e969410677e59784f16a1119 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -398,7 +398,15 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-i-ve-finally-launched-my-website-this-website-still-under-construction-️",
+            },},{id: "news-a-long-announcement-with-details",
+          title: 'A long announcement with details',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_2/";
+            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-ve-finally-launched-my-website-this-website-still-under-construction-️",
           title: 'I’ve finally launched my website (this website)! ✨ Still under construction 🛠️',
           description: "",
           section: "News",},{id: "news-new-preprint-online-link",
